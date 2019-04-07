5f6eec212786feb04c7463f7923249b6524d8299
PequalsNPaspirant <marutimhetre@gmail.com>
2019-04-06 21:47:11 -0400

    Renamed files

--- src/_MM_DesignPatterns_main.cpp
+++ src/_MM_DesignPatterns_main.cpp
@@ -2,19 +2,14 @@
 
 #include <iostream>
 
-#include "WindowsUtils/WindowsUtils.h"
-#include "Timer/Timer.h"
 #include "MM_UnitTestFramework/MM_UnitTestFramework.h"
-
 using namespace mm;
 
 int main(int argc, char* argv[])
 {
-	MaximizeWindow();				
-
 	MM_RUN_UNIT_TESTS
 
-	cout << "\n\n\n" << Timer::getCurrentLocalTimeInNanoSeconds() << " CONGRATULATIONS!!! End of program reached successfully.\n\n\n";
+	cout << "\n\n\n" << " CONGRATULATIONS!!! End of program reached successfully.\n\n\n";
 
 	std::cin.get();
 	return 0;
